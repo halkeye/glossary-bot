10900ba737b262022810e926a65404a91fa8ed03
Gavin Mogan <git@gavinmogan.com>
2023-10-18 18:07:45 -0700

    chore: add releases to release notes

--- release.config.cjs
+++ release.config.cjs
@@ -46,6 +46,12 @@ module.exports = {
         ],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        "addReleases": true
+      }
     ]
   ]
 }
