ede76da139cccb0243224c5c6a4f210d0eac352e
Gavin Mogan <git@gavinmogan.com>
2025-08-16 10:09:41 -0700

    chore: ruff cleanups

--- alembic/versions/04b2d51ca468_undo_postgres_items.py
+++ alembic/versions/04b2d51ca468_undo_postgres_items.py
@@ -5,19 +5,21 @@ Revises: 201bae6698f6
 Create Date: 2023-08-16 19:46:06.705898
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '04b2d51ca468'
-down_revision = '201bae6698f6'
+revision = "04b2d51ca468"
+down_revision = "201bae6698f6"
 branch_labels = None
 depends_on = None
 
+
 def upgrade() -> None:
     db_bind = op.get_bind()
-    if db_bind.engine.name != 'postgresql':
+    if db_bind.engine.name != "postgresql":
         return
 
     #
@@ -25,67 +27,110 @@ def upgrade() -> None:
     #
 
     # drop the varchar_pattern_ops index
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP INDEX IF EXISTS ix_definitions_term;
-    '''))
+    """
+        )
+    )
     # re-create the standard index
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         CREATE INDEX ix_definitions_term ON definitions(term);
-    '''))
+    """
+        )
+    )
 
     #
     # Remove support for full-text search on terms and definitions
     #
 
     # drop the tsv_search column from the definitions table
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         ALTER TABLE definitions DROP COLUMN IF EXISTS tsv_search;
-    '''))
+    """
+        )
+    )
 
     # drop the search trigger and function
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP TRIGGER IF EXISTS tsvupdate_definitions_trigger ON definitions;
-    '''))
-    db_bind.execute(sa.sql.text('''
+    """
+        )
+    )
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP FUNCTION IF EXISTS definitions_search_trigger();
-    '''))
+    """
+        )
+    )
 
     # drop the index
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP INDEX IF EXISTS ix_definitions_tsv_search;
-    '''))
+    """
+        )
+    )
+
 
 def downgrade() -> None:
     db_bind = op.get_bind()
-    if db_bind.engine.name != 'postgresql':
+    if db_bind.engine.name != "postgresql":
         return
     #
     # Replace the index for terms with one including varchar_pattern_ops
     #
 
     # drop the old index
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP INDEX IF EXISTS ix_definitions_term;
-    '''))
+    """
+        )
+    )
     # create the new index
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         CREATE INDEX ix_definitions_term ON definitions (term varchar_pattern_ops);
-    '''))
+    """
+        )
+    )
 
     #
     # Support full-text search on terms and definitions
     #
 
     # add the tsv_search column to the definitions table
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         ALTER TABLE definitions ADD COLUMN tsv_search tsvector;
-    '''))
+    """
+        )
+    )
 
     # set up a trigger to populate tsv_search when records are created or altered
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP FUNCTION IF EXISTS definitions_search_trigger();
-    '''))
-    db_bind.execute(sa.sql.text('''
+    """
+        )
+    )
+    db_bind.execute(
+        sa.sql.text(
+            """
         CREATE FUNCTION definitions_search_trigger() RETURNS trigger AS $$
         begin
           new.tsv_search :=
@@ -94,24 +139,46 @@ def downgrade() -> None:
           return new;
         end
         $$ LANGUAGE plpgsql;
-    '''))
+    """
+        )
+    )
 
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP TRIGGER IF EXISTS tsvupdate_definitions_trigger ON definitions;
-    '''))
-    db_bind.execute(sa.sql.text('''
+    """
+        )
+    )
+    db_bind.execute(
+        sa.sql.text(
+            """
         CREATE TRIGGER tsvupdate_definitions_trigger BEFORE INSERT OR UPDATE ON definitions FOR EACH ROW EXECUTE PROCEDURE definitions_search_trigger();
-    '''))
+    """
+        )
+    )
 
     # create an index for tsv_search
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         DROP INDEX IF EXISTS ix_definitions_tsv_search;
-    '''))
-    db_bind.execute(sa.sql.text('''
+    """
+        )
+    )
+    db_bind.execute(
+        sa.sql.text(
+            """
         CREATE INDEX ix_definitions_tsv_search ON definitions USING gin(tsv_search);
-    '''))
+    """
+        )
+    )
 
     # populate tsv_search for existing records
-    db_bind.execute(sa.sql.text('''
+    db_bind.execute(
+        sa.sql.text(
+            """
         UPDATE definitions SET tsv_search = setweight(to_tsvector('pg_catalog.english', COALESCE(term,'')), 'A') || setweight(to_tsvector('pg_catalog.english', COALESCE(definition,'')), 'B');
-    '''))
+    """
+        )
+    )
--- alembic/versions/4614666a279f_renamed_column_user_to_user_name.py
+++ alembic/versions/4614666a279f_renamed_column_user_to_user_name.py
@@ -6,20 +6,40 @@ Create Date: 2015-06-29 20:44:11.117613
 
 """
 
-# revision identifiers, used by Alembic.
-revision = '4614666a279f'
-down_revision = '578b43a08697'
-
 from alembic import op
 import sqlalchemy as sa
 
 
+# revision identifiers, used by Alembic.
+revision = "4614666a279f"
+down_revision = "578b43a08697"
+
 
 def upgrade():
-    op.alter_column('definitions', 'user', new_column_name='user_name', existing_type=sa.Unicode(255))
-    op.alter_column('interactions', 'user', new_column_name='user_name', existing_type=sa.Unicode(255))
+    op.alter_column(
+        "definitions",
+        "user",
+        new_column_name="user_name",
+        existing_type=sa.Unicode(255),
+    )
+    op.alter_column(
+        "interactions",
+        "user",
+        new_column_name="user_name",
+        existing_type=sa.Unicode(255),
+    )
 
 
 def downgrade():
-    op.alter_column('definitions', 'user_name', new_column_name='user', existing_type=sa.Unicode(255))
-    op.alter_column('interactions', 'user_name', new_column_name='user', existing_type=sa.Unicode(255))
+    op.alter_column(
+        "definitions",
+        "user_name",
+        new_column_name="user",
+        existing_type=sa.Unicode(255),
+    )
+    op.alter_column(
+        "interactions",
+        "user_name",
+        new_column_name="user",
+        existing_type=sa.Unicode(255),
+    )
